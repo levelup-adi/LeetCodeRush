54d601a9ded8abdb9abaadf27882add6482d3586
ADITYA RAJ SHARMA <recurziveaditya@gmail.com>
2026-08-26 23:42:10 +0530

    Time: 22 ms (59.99%) | Memory: 17.6 MB (24.37%) - LeetSync

---
+++ 18-4sum/4sum.cpp
@@ -0,0 +1,66 @@
+class Solution {
+public:
+    vector<vector<int>> fourSum(vector<int>& nums, int target) {
+
+        vector<vector<int>> resset;
+        int n = nums.size();
+
+        sort(nums.begin(), nums.end());
+
+        for(int i = 0; i < n - 3; i++) {
+
+            // Skip duplicate first elements
+            if(i > 0 && nums[i] == nums[i - 1])
+                continue;
+
+            for(int j = i + 1; j < n - 2; j++) {
+
+                // Skip duplicate second elements
+                if(j > i + 1 && nums[j] == nums[j - 1])
+                    continue;
+
+                int left = j + 1;
+                int right = n - 1;
+
+                while(left < right) {
+
+                    long long sum = (long long)nums[i]
+                                  + nums[j]
+                                  + nums[left]
+                                  + nums[right];
+
+                    if(sum == target) {
+
+                        resset.push_back({
+                            nums[i],
+                            nums[j],
+                            nums[left],
+                            nums[right]
+                        });
+
+                        left++;
+                        right--;
+
+                        // Skip duplicates for left
+                        while(left < right && nums[left] == nums[left - 1])
+                            left++;
+
+                        // Skip duplicates for right
+                        while(left < right && nums[right] == nums[right + 1])
+                            right--;
+                    }
+
+                    else if(sum < target) {
+                        left++;
+                    }
+
+                    else {
+                        right--;
+                    }
+                }
+            }
+        }
+
+        return resset;
+    }
+};
